19f1b03583c4ca58bc34ab545a18a218cdc6f036
Kala Elam <speechgirl1505@gmail.com>
2019-11-08 10:00:38 -0600

    add index.js

--- models/article.js
+++ models/article.js
@@ -25,9 +25,8 @@
       ref: "Note"
     }
   });
-  
   // This creates our model from the above schema, using mongoose's model method
   var Article = mongoose.model("Article", ArticleSchema);
   
   // Export the Article model
-  module.exports = Article;
+ module.exports = Article;
