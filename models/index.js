59ad650b58a21f1fcd55bfd7283ad98dc5b7258d
Kala Elam <speechgirl1505@gmail.com>
2019-11-11 14:06:41 -0600

    fix?

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 // Exporting an object containing all of our models
 module.exports = {
-Article: require("./Article"),
-Note: require("./Note")
+  Note: require("./Note"),
+  Article: require("./Article")
 };
